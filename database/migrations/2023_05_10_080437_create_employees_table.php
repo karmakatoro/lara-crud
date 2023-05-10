e2eda78d2ba605cd1e9a1ed1af11d54dd60a7cc7
karmakatoro <karmakatoro@gmail.com>
2023-05-10 10:08:11 +0200

    fill migration and migrate

--- database/migrations/2023_05_10_080437_create_employees_table.php
+++ database/migrations/2023_05_10_080437_create_employees_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('post');
+            $table->string('avatar');
             $table->timestamps();
         });
     }
