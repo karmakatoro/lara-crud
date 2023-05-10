e948db80028576cb15f56d10bd32acf765a735c0
karmakatoro <karmakatoro@gmail.com>
2023-05-10 10:31:39 +0200

    add assets(css and js)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EmployeeController;
 use App\Http\Livewire\StudentsComponent;
 use Illuminate\Support\Facades\Route;
 
@@ -14,7 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [EmployeeController::class, 'index']);
 Route::get('students', StudentsComponent::class);
